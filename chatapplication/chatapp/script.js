1f868cb1b809ee5f78ed2c218a90a8e3bfd06bab
Vaibhav Agarwal <agvaibhav213@gmail.com>
2025-01-13 22:19:51 +0530

    new

---
+++ chatapplication/chatapp/script.js
@@ -0,0 +1,24 @@
+function sizeadjust(){
+    const width=document.innerWidth;
+    const body=document.body;
+    if(width<=600){
+        body.style.transform="scale(0.5)";
+    }else if(width<=700){
+        body.style.transform="scale(0.75)";
+    }else if(width<=767){
+        body.style.transform="scale(0.8)";
+    }else if(width<=1600){
+        body.style.transform="scale(0.9)";
+    }else{
+        body.style.transform="scale(1.0)";
+    }
+}
+
+function togglemenu(){
+    const leftmenubutton=document.getElementById("togglemenu");
+    leftmenubutton.classList.toggle('hidden');
+}
+
+document.getElementById("togglemenu").addEventListener('click',togglemenu);
+window.addEventListener('resize',sizeadjust);
+window.addEventListener('load',sizeadjust);
