185462ab48f1bb1d14d4d356dfdd235e1721f55e
Vaibhav Agarwal <108589180+vaspooki@users.noreply.github.com>
2025-01-16 16:51:47 +0530

    Add files via upload

---
+++ S3Upload.py
@@ -0,0 +1,26 @@
+import boto3
+
+AWS_S3_BUCKET_NAME = 'pdfdatastorage'
+AWS_REGION = 'eu-north-1'
+AWS_ACCESS_KEY = 'AKIASDRANKD6YWBT64EK'
+AWS_SECRET_KEY = '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'
+
+LOCAL_FILE = 'C:/Users/agvai/Downloads/Assignment_Tasks.docx.pdf'
+NAME_FOR_S3 = 'Assignment_Tasks.pdf'
+
+def main():
+    print('in main method')
+
+    s3_client = boto3.client(
+        service_name='s3',
+        region_name=AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY
+    )
+
+    response = s3_client.upload_file(LOCAL_FILE, AWS_S3_BUCKET_NAME, NAME_FOR_S3)
+
+    print(f'upload_log_to_aws response: {response}')
+
+if __name__ == '__main__':
+    main()
